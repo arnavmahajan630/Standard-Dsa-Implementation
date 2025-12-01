e801aef187d03dfd94b8ba878241f5c1c3360c01
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2025-12-01 19:36:58 +0530

    feat/topo-sort-graph

---
+++ Graphs/Algorithms/Topo Sort/topo_sort.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+const int N = 1e5 + 3;
+vector<int>graph[N];
+
+void TopoSort(int r, vector<bool>&visited, stack<int>&s) {
+    visited[r] = true;
+
+    for(auto x : graph[r]) {
+        if(!visited[x])TopoSort(x, visited, s);
+    }
+    // the neighbours for a specific node are put first. so we can gaurentee that if there is an edge 
+    // the v is always ahead of u. Stack ensures that for us.
+    s.push(r);
+}
+
+
+void inputG(int m) {
+    for(int i=0; i < m; ++i) {
+        int u,v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        if(u == v)continue;
+    }
+
+}
+int main() {
+    cout << "Ensure you enter Directed Acyclic Graph" << endl;
+    cout << "Enter the number of vertices and edges: " << endl;
+    int n, m;
+    cin >> n >> m;
+
+    cout << "For next m lines enter the nodes where edge is present: \n" << endl;
+    inputG(m);
+
+    vector<bool>visited(n, false);
+    stack<int>s;
+    for(int i=0; i < n; ++i) {
+        if(!visited[i])TopoSort(i, visited, s);
+    }
+
+    while(!s.empty()) {
+        cout << s.top() << endl;
+        s.pop();
+    }
+    return ;
+}
