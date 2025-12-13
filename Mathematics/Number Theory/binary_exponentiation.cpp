5c9bee82cc2e2474bce1735ab45f74e7601f7745
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2025-12-13 19:06:28 +0530

    feat/iterative_exp

--- Mathematics/Number Theory/binary_exponentiation.cpp
+++ Mathematics/Number Theory/binary_exponentiation.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Binary EXP with Modulo
 const int M = 1e9 + 7;
 
 int recursive(int a, int b) {
@@ -12,12 +13,19 @@ int recursive(int a, int b) {
 }
 
 int iterative(int a, int b) {
-    
+    int ans = 1;
+    while(b > 0) { // if unit bit is set multiply with base
+        if(b&1)ans  = (ans * 1LL * a)%M;
+        a = (a*a * 1LL )%M;
+        b >>=1;
+    }
+    return ans;
 }
 
 
 int main() {
 
    cout << recursive(2, 3) << endl;
+   cout << iterative(2, 3) << endl;
     return 0;
 }
